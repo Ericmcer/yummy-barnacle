1fc71357ba84a531da47c51ab2e937abf9e1a03b
Ericmcer <emcelyea@horizon.csueastbay.edu>
2015-09-27 14:46:25 -0500

    updating math game

--- javascript/stuffModule.js
+++ javascript/stuffModule.js
@@ -4,7 +4,7 @@ angular.module('stuff', [])
 	.controller('StuffCtrl', ['$scope',
 		function($scope){
 			$scope.switchProject = function(url){
-				$scope.currentProject = 'projects/mathAssassin.html';
+				$scope.currentProject = url;
 			}
 		}
 	])
